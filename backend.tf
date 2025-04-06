92cfb340ddbd2427d1f22975b246ec33fd4dd8cf
tusharpatilITAWS <102217272+tusharpatilITAWS@users.noreply.github.com>
2025-04-07 00:35:35 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-sample-batch-26"
+    bucket = "tushar-project-terraform-aws-devops"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
