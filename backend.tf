f9af7fa26894960d8ded0d716681e995ece8cb88
tusharpatilITAWS <102217272+tusharpatilITAWS@users.noreply.github.com>
2025-04-07 00:39:19 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "tushar-project-terraform-aws-devops"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "mydynamotable-tushar"
   }
 }
